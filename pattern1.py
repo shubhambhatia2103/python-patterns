6b3076dd3a0198f429f6e69fe50d58b9fcf6834d
shubhambhatia2103 <shubhambhatia2103@gmail.com>
2024-06-19 11:23:17 +0530

    square

--- pattern1.py
+++ pattern1.py
@@ -1,11 +1,11 @@
-####
-####
-####
-####
+# # # # 
+# # # #
+# # # #
+# # # #
 
 
 for i in range(4):
     for j in range(4):
-        print("#", end="")
+        print("#", end=" ")
     print()
     
